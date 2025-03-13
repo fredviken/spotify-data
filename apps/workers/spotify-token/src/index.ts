645e0c134180b6ddc4a1ec8b31bc4832c38f0c30
FredViken <fredrik.mjos@hotmail.com>
2025-03-13 23:40:49 +0100

    feat(spotify-token): update worker configuration and implement access token workflow

--- apps/workers/spotify-token/src/index.ts
+++ apps/workers/spotify-token/src/index.ts
@@ -1,14 +1,35 @@
 import { WorkflowEntrypoint, WorkflowStep, WorkflowEvent } from 'cloudflare:workers';
 
+type SpotifyResponse = {
+	clientId: string;
+	accessToken: string;
+	accessTokenExpirationTimestampMs: number;
+	isAnonymous: boolean;
+	totpValidity: boolean;
+};
+
 export default {
 	async scheduled(event, env, ctx): Promise<void> {
-		const res = await fetch('https://open.spotify.com/get_access_token?reason=init&productType=web-player');
-
-		const data = await res.json();
-
-		// const accessToken = data.accessToken;
-
-
+		// trigger workflow
+		env.WEB_TOKEN_WORKFLOW.create();
 	},
 } satisfies ExportedHandler<Env>;
 
+export class WebTokenWorkflow extends WorkflowEntrypoint<Env, Params> {
+	async run(_: WorkflowEvent<Params>, step: WorkflowStep) {
+		// fetch access token
+		let accessToken = await step.do('fetch access token', async () => {
+			console.log('fetching access token');
+			const res = await fetch('https://open.spotify.com/get_access_token?reason=init&productType=web-player');
+			if (!res.ok) throw new Error(`Failed to fetch access token: ${res.statusText}`);
+			const data = (await res.json()) as SpotifyResponse;
+			return data.accessToken;
+		});
+
+		// save access token
+		await step.do('save access token', async () => {
+			console.log('saving access token');
+			await this.env.SPOTIFY_WEB_TOKEN.put('accessToken', accessToken);
+		});
+	}
+}
